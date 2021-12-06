41ea1108407995c3e172fc0ee85362ec05e1156d
Dwi <dwijulianto16@gmail.com>
2021-12-06 14:22:26 +0700

    add index

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	include_once("content/controller/base_setting.php");
+
+	echo '<script type="text/javascript">location.replace("'.base_url().'content/controller/dashboard.php");</script>';
+?>
